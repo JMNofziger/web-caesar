27c927ddb9cef548afa3be30ee97e7cca5b123e9
JMNofziger <jacob.nofziger@gmail.com>
2017-06-14 20:23:36 -0700

    Added form content but stopping to read more about templating

--- main.py
+++ main.py
@@ -4,8 +4,26 @@ import html
 app=Flask(__name__)
 app.config['DEBUG'] = True
 
+def add_form():
+    form_content="""
+    <form action="{method}" method="post">
+        <label>
+            I want to cross off
+            <select name="crossed-off-movie"/>
+                <option value="Star Wars">Star Wars</option>
+                <option value="My Favorite Martian">My Favorite Martian</option>
+                <option value="The Avengers">The Avengers</option>
+                <option value="The Hitchhiker's Guide To The Galaxy">The Hitchhiker's Guide To The Galaxy</option>
+            </select>
+            from my watchlist.
+        </label>
+        <input type="submit" value="Cross It Off"/>
+    </form>
+    """
+
+
 @app.route("/")
 def index():
-    return "First line baby!"
+    
 
 app.run()
